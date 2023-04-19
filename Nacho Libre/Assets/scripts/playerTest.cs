dc72f91ff948c62fbde02bd62b0cb06414f816d0
Hodak Nak <htoytoytok@gmail.com>
2023-04-19 14:37:06 -0700

    everything

---
+++ Nacho Libre/Assets/scripts/playerTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playerTest : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent agent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
