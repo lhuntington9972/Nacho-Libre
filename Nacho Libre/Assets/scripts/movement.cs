459d9fdcdda40e1785433aab93f41c5d00ad806a
Luke Huntington <lhuntington9972@student.pps.net>
2023-05-08 15:28:54 -0700

    TASKS AND TEXtS

--- Nacho Libre/Assets/scripts/movement.cs
+++ Nacho Libre/Assets/scripts/movement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+using TMPro;
 
 public class movement : MonoBehaviour
 {
@@ -15,9 +17,19 @@ public class movement : MonoBehaviour
 
     GameObject indicator;
 
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI taskText;
+    private float game1Countdown;
+    private bool game1Finished;
+    private string currentTaskString;
+    private int currentTask;
+    
+
     private void Start()
     {
         indicator = Instantiate(indicatorPrefab);
+        game1Countdown = 10f;
+        currentTask = 1;
     }
 
     private void Update()
@@ -33,6 +45,27 @@ public class movement : MonoBehaviour
                 indicator.transform.position = hit.point;
             }
         }
+
+        if (flyerScore < 2 && currentTask == 1)
+        {
+            scoreText.text = "Score: " + flyerScore;
+        } else if (flyerScore >= 2 && currentTask == 1)
+        {
+            StartCoroutine(game1FinishCountdown());
+        }
+
+        taskText.text = "Current task: " + currentTaskString;
+
+        if (currentTask == 0)
+        {
+            currentTaskString = "Find something else do to!";
+        } else if (currentTask == 1)
+        {
+            currentTaskString = "Eradicate uninclusive flyers!";
+        } else if (currentTask == 2)
+        {
+            currentTaskString = "SLAM GORILLAS!!!";
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -41,6 +74,23 @@ public class movement : MonoBehaviour
         {
             flyerScore += 1;
             Debug.Log("your score is now: " + flyerScore); 
+        } else if (other.tag == "taskTwoBarrier")
+        {
+            currentTask = 2; 
+        } else if (other.tag == "taskOneBarrier" && game1Finished == false)
+        {
+            currentTask = 1; 
         }
+    }
+
+    IEnumerator game1FinishCountdown()
+    {
+        currentTask = 0;
+        game1Finished = true; 
+        scoreText.text = "You completed the first task!";
+        yield return new WaitForSeconds(game1Countdown);
+        //Destroy(scoreText);
+        scoreText.text = "";
+        
     }
 }
